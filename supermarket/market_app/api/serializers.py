352f2b1c57a6f3c41593e544c04834104dcedee4
Simeon199 <sim.ki@outlook.de>
2025-09-17 14:33:13 +0200

    feat: productserializer added - changes files are views.py and serializers.py

--- supermarket/market_app/api/serializers.py
+++ supermarket/market_app/api/serializers.py
@@ -64,11 +64,35 @@ class SellerCreateSerializer(serializers.Serializer):
 class ProductSerializer(serializers.Serializer):
      id = serializers.IntegerField(read_only=True)
      name = serializers.CharField(max_length=255)
-     description = serializers.CharField(max_length=255)
+     description = serializers.CharField()
      price = serializers.DecimalField(max_digits=100, decimal_places=2)
-     markets = serializers.ListField(child=serializers.IntegerField(), write_only=True)
-     sellers = serializers.ListField(child=serializers.IntegerField(), write_only=True)
+     market = serializers.IntegerField()
+     seller = serializers.IntegerField()
 
+     # markets = serializers.ListField(child=serializers.IntegerField(), write_only=True)
+     # sellers = serializers.ListField(child=serializers.IntegerField(), write_only=True)
+
+
+     def create(self, validated_data):
+          # Fetch related objects using the provided IDs
+          market = Market.objects.get(pk=validated_data['market'])
+          seller = Seller.objects.get(pk=validated_data['seller'])
+          # Remove IDs from validated_data and add actual objects
+          validated_data['market'] = market
+          validated_data['seller'] = seller
+          return Product.objects.create(**validated_data)
+     
+     def update(self, instance, validated_data):
+          instance.name = validated_data.get('name', instance.name)
+          instance.description = validated_data.get('description', instance.description)
+          instance.price = validated_data.get('price', instance.price)
+          if 'market' in validated_data:
+               instance.market = Market.objects.get(pk=validated_data['market'])
+          if 'seller' in validated_data:
+               instance.seller = Seller.objects.get(pk=validated_data['seller'])
+          instance.save()
+          return instance
+     
 # {
 #      "name": "Seller1",
 #      "contact_info": "Seller1@example.com",
--- supermarket/market_app/api/views.py
+++ supermarket/market_app/api/views.py
@@ -1,7 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import MarketSerializer, SellerDetailSerializer, SellerCreateSerializer
+from .serializers import MarketSerializer, SellerDetailSerializer, SellerCreateSerializer, ProductSerializer
 from market_app.models import Market, Seller, Product
 
 @api_view(['GET', 'POST'])
@@ -55,6 +55,39 @@ def sellers_view(request):
             return Response(serializer.data)
         else:
             return Response(serializer.errors)
+        
+@api_view(['GET', 'POST'])
+def products_view(request):
+    if request.method == 'GET':
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+    if request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+@api_view(['GET', 'PUT', 'DELETE'])
+def product_detail_view(request, pk):
+    try:
+        product = Product.objects.get(pk=pk)
+    except Product.DoesNotExist:
+        return Response({'error': 'Product not found'}, status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+    if request.method == 'PUT':
+        serializer = ProductSerializer(product, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    if request.method == 'DELETE':
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
         
 # @api_view(['GET', 'POST'])
 # def products_view(request):
