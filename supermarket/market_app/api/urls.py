d0508645e7f9c59a9d938699093d9659e6257403
Simeon199 <sim.ki@outlook.de>
2025-09-27 20:24:36 +0200

    feat: revised urls.py and views.py - now working with mixins instead of modelserializers

--- supermarket/market_app/api/urls.py
+++ supermarket/market_app/api/urls.py
@@ -1,12 +1,12 @@
 
 from django.urls import path
-from .views import markets_view, market_single_view, sellers_view, products_view, sellers_single_view, product_detail_view
+from .views import MarketsView, MarketDetailView, SellersView, ProductsView, SellerDetailView, ProductDetailView
 
 urlpatterns = [
-    path('market/', markets_view),
-    path('market/<int:pk>/', market_single_view, name='market-detail'),
-    path('seller/', sellers_view),
-    path('seller/<int:pk>/', sellers_single_view, name='seller-single'),
-    path('products/', products_view),
-    path('products/<int:pk>', product_detail_view, name='product-detail')
+    path('market/', MarketsView.as_view()),
+    path('market/<int:pk>/', MarketDetailView.as_view(), name='market-detail'),
+    path('seller/', SellersView.as_view()),
+    path('seller/<int:pk>/', SellerDetailView.as_view(), name='seller-single'),
+    path('products/', ProductsView.as_view()),
+    path('products/<int:pk>', ProductDetailView.as_view(), name='product-detail')
 ]
--- supermarket/market_app/api/views.py
+++ supermarket/market_app/api/views.py
@@ -1,143 +1,92 @@
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework import status
+# from rest_framework.decorators import api_view
+# from rest_framework.response import Response
+# from rest_framework import status
 from .serializers import MarketSerializer, ProductSerializer, SellerSerializer
-from django.shortcuts import get_object_or_404
+# from django.shortcuts import get_object_or_404
 from market_app.models import Market, Seller, Product
+from rest_framework import mixins
+from rest_framework import generics
+from rest_framework.generics import RetrieveUpdateDestroyAPIView
 
-@api_view(['GET', 'POST'])
-def markets_view(request):
-    if request.method == 'GET':
-        markets = Market.objects.all()
-        serializer = MarketSerializer(
-            markets, 
-            many=True, 
-            context={'request': request},
-            fields=('id', 'name', 'net_worth')
-        )
-        return Response(serializer.data)
-
-    if request.method == 'POST':
-        serializer = MarketSerializer(
-            data=request.data, 
-            context={'request': request}
-        )
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        else:
-            return Response(serializer.errors)
-
-@api_view(['GET', 'PUT', 'PATCH', 'DELETE'])
-def market_single_view(request, pk):
-    market = get_object_or_404(Market, pk=pk)
-    if request.method == 'GET':
-        serializer = MarketSerializer(
-            market, 
-            context={'request': request}
-        )
-        return Response(serializer.data)
-    if request.method in ['PUT', 'PATCH']:
-        serializer = MarketSerializer(
-            market,
-            context={'request': request}, 
-            data=request.data, 
-            partial=True
-        )
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    if request.method == 'DELETE':
-        market.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class MarketsView(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    generics.GenericAPIView
+    ):
     
-@api_view(['GET', 'POST'])
-def sellers_view(request):
-    if request.method == 'GET':
-        sellers = Seller.objects.all()
-        serializer = SellerSerializer(
-            sellers, 
-            many=True, 
-            context={'request': request}
-        )
-        return Response(serializer.data)
+    queryset = Market.objects.all()
+    serializer_class = MarketSerializer
 
-    if request.method == 'POST':
-        serializer = SellerSerializer(
-            data=request.data, 
-            context={'request': request}
-        )
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        else:
-            return Response(serializer.errors)
-
-@api_view(['GET', 'PUT', 'PATCH', 'DELETE'])
-def sellers_single_view(request, pk):
-    seller = get_object_or_404(Seller, pk=pk)
-    if request.method == 'GET':
-        serializer = SellerSerializer(
-            seller, 
-            context={'request': request}
-        )
-        return Response(serializer.data)
-    if request.method in ['PUT', 'PATCH']:
-        serializer = SellerSerializer(
-            seller, 
-            context={'request': request}, 
-            data=request.data, 
-            partial=True
-        )
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    if request.method == 'DELETE':
-        seller.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-@api_view(['GET', 'POST'])
-def products_view(request):
-    if request.method == 'GET':
-        products = Product.objects.all()
-        serializer = ProductSerializer(
-            products, 
-            many=True, 
-            context={'request': request}
-        )
-        return Response(serializer.data)
-    if request.method == 'POST':
-        serializer = ProductSerializer(
-            data=request.data, 
-            context={'request': request}
-        )
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
     
-@api_view(['GET', 'PUT', 'PATCH', 'DELETE'])
-def product_detail_view(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    if request.method == 'GET':
-        serializer = ProductSerializer(
-            product, 
-            context={'request': request}
-        )
-        return Response(serializer.data)
-    if request.method in ['PUT', 'PATCH']:
-        serializer = ProductSerializer(
-            product, 
-            context={'request': request}, 
-            data=request.data, 
-            partial=True
-        )
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    if request.method == 'DELETE':
-        product.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+    
+class SellersView(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    generics.GenericAPIView
+):
+    queryset = Seller.objects.all()
+    serializer_class = SellerSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+    
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+    
+class ProductsView(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    generics.GenericAPIView
+):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+    
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+    
+class MarketDetailView(RetrieveUpdateDestroyAPIView):
+    queryset = Market.objects.all()
+    serializer_class = MarketSerializer
+    lookup_field = 'pk'
+
+class SellerDetailView(RetrieveUpdateDestroyAPIView):
+    queryset = Seller.objects.all()
+    serializer_class = SellerSerializer
+    lookup_field = 'pk'    
+
+class ProductDetailView(RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'pk'
+
+
+# @api_view(['GET', 'PUT', 'PATCH', 'DELETE'])
+# def product_detail_view(request, pk):
+#     product = get_object_or_404(Product, pk=pk)
+#     if request.method == 'GET':
+#         serializer = ProductSerializer(
+#             product, 
+#             context={'request': request}
+#         )
+#         return Response(serializer.data)
+#     if request.method in ['PUT', 'PATCH']:
+#         serializer = ProductSerializer(
+#             product, 
+#             context={'request': request}, 
+#             data=request.data, 
+#             partial=True
+#         )
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#     if request.method == 'DELETE':
+#         product.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
